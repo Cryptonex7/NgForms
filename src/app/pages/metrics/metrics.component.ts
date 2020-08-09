24d3b56046e919ad231886502f53d5c74135229c
Anujeet Chatterjee <canujeet@vmware.com>
2020-08-09 23:35:15 +0530

    all citizen datagrid

--- src/app/pages/metrics/metrics.component.ts
+++ src/app/pages/metrics/metrics.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Citizen } from '../../models/Citizen.model';
 @Component({
   selector: 'app-metrics',
   templateUrl: './metrics.component.html',
-  styleUrls: ['./metrics.component.scss']
+  styleUrls: ['./metrics.component.scss'],
 })
 export class MetricsComponent implements OnInit {
-
-  constructor() { }
+  citizens: Citizen[] = [];
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get('https://crypto-spring-boot.herokuapp.com/citizen')
+      .subscribe((res: Citizen[]) => {
+        console.table(res);
+        this.citizens = res;
+      });
   }
-
 }
